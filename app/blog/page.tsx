df838c736ebeee9039b73b99ddaa550075f0de83
SuitBridge <connect.suitbridge@gmail.com>
2026-07-02 01:05:26 +0600

    Fix blog page module

---
+++ app/blog/page.tsx
@@ -0,0 +1,21 @@
+export default function BlogPage(){
+
+return (
+
+<main>
+
+<h1>
+Industrial Guides
+</h1>
+
+
+<p>
+Latest engineering articles and equipment insights here.
+</p>
+
+
+</main>
+
+)
+
+}
